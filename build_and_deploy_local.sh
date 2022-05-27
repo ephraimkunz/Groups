83c4bfa82354b396636d21665b9a1c0b8e5faffc
Ephraim Kunz <ephraimkunz@me.com>
2022-05-27 07:38:03 -0700

    Install wasm-pack in build script if not already installed

--- build_and_deploy_local.sh
+++ build_and_deploy_local.sh
@@ -1,4 +1,4 @@
 ./build_local.sh
 
 # Run the local webserver for testing.
-cargo run --bin groups_server_local
+cargo run --release --bin groups_server_local
--- build_local.sh
+++ build_local.sh
@@ -1,4 +1,4 @@
 ./package_wasm.sh
 
 # Build actual site
-cargo build --bin groups_server_local
+cargo build --release --bin groups_server_local
