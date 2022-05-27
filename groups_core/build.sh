a50af2c0a389dea8d3893981ab82e5eaf9e450a7
Ephraim Kunz <ephraimkunz@me.com>
2022-05-27 07:38:12 -0700

    Install wasm-back in build script

--- groups_core/build.sh
+++ groups_core/build.sh
@@ -1,1 +1,7 @@
+if ! command -v wasm-pack &> /dev/null
+then
+    echo "wasm-pack could not be found, installing"
+    curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+fi
+
 wasm-pack build --profiling --target web
